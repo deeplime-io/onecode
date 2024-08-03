5004a449f1754627246d999602f55c294bc67c3c
theweaklink <cavelius@protonmail.com>
2024-08-03 23:17:44 +0200

    adding new functions to get all modules imported by script and to check whether they are in the Python env

--- onecode/utils/module.py
+++ onecode/utils/module.py
@@ -4,11 +4,16 @@
 import importlib
 import os
 import sys
+from glob import iglob
 from pathlib import Path
 from types import ModuleType
-from typing import Optional
+from typing import Dict, List, Optional
+
+from pycg.pycg import CallGraphGenerator
+from pycg.utils.constants import CALL_GRAPH_OP
 
 from ..base.decorator import check_type
+from ..base.logger import Logger
 
 
 @check_type
@@ -47,3 +52,57 @@ def register_ext_module(
         spec.loader.exec_module(module)
 
         return module
+
+
+def get_imported_modules(scripts_folder: str = os.getcwd()) -> List[str]:
+    """
+    Get the names of all modules imported by the Python scripts present in the given folder.
+
+    Args:
+        scripts_folder: folder containing the python. Defaults to the working directory.
+
+    Returns:
+        List of modules names imported by the Python scripts.
+
+    """
+    entry_files = list(iglob(os.path.join(scripts_folder, '**', '*.py'), recursive=True))
+    cg = CallGraphGenerator(
+        entry_files,
+        scripts_folder,
+        0,
+        CALL_GRAPH_OP
+    )
+    cg.analyze()
+
+    return list(cg.output_external_mods().keys())
+
+
+def check_modules_in_env(
+    modules: List[str],
+    verbose: bool = False
+) -> Dict[str, bool]:
+    """
+    Check whether all imported modules are present in
+
+    !!! info
+        It is not required to call this function explicitely. It is already done automatically as
+        part of the OneCode project under `main.py`.
+
+    Args:
+        scripts_folder: Path to the root of the OneCode project.
+
+    Returns:
+        The module if it contains Python code, otherwise None.
+
+    """
+    mods = {}
+    for m in modules:
+        mod_ok = importlib.util.find_spec(m) is not None or m == '<builtin>'
+        mods[m] = mod_ok
+        if verbose:
+            if mod_ok:
+                Logger.info(f'{m} ✅')
+            else:
+                Logger.warning(f'{m} 💥')
+
+    return mods
