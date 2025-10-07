47636b56fab37928ca9378a71fdb4543ea7393bd
theweaklink <cavelius@protonmail.com>
2025-10-07 09:17:52 +0200

    update version to rc1

--- onecode/__init__.py
+++ onecode/__init__.py
@@ -1,7 +1,7 @@
 # SPDX-FileCopyrightText: 2023-2024 DeepLime <contact@deeplime.io>
 # SPDX-License-Identifier: MIT
 
-__version__ = "2.0.0.pre"
+__version__ = "2.0.0rc1"
 
 from .base import *
 from .elements import *
